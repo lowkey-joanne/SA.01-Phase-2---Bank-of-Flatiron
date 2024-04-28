6e755f418fc179dcfa71c56d785b5b6e5b60ca7e
joanne <joannembula33@gmail.com>
2024-04-28 20:28:21 +0300

    done

--- src/Components/ SearchBox.jsx
+++ src/Components/ SearchBox.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { useState } from 'react';
 
-function SearchBox() {
+function SearchBox() {}
     return (
             <input type="text" className="SearchInput" placeholder="Search Your Recents Transactions..."/>
 
     )
+function SearchBox({instateRecords, funcSetData}) {
+    const [searchValue, setSearch] = useState("")
+
+function handleSearch(e){
+    setSearch(e.target.value)
+
+    const filteredRecords = instateRecords.filter(indata => 
+        indata.description.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    funcSetData(filteredRecords);
+}
+
+    return (<>
+        <input 
+            type="text" 
+            className="SearchInput"  
+            placeholder="Search Your Recents Transactions..."
+            value={searchValue}
+            onChange={handleSearch}
+        />
+    </>
+ )
 }
 
 export default SearchBox;
+  
